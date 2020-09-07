cbeb8a8e04c3cb197b3f3b2656e4a67b7bc0ed04
KiptanRory <70909955+KiptanRory@users.noreply.github.com>
2020-09-07 19:07:39 +0200

    Update HalloRocDev.java

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Heyy dames en heren!");
+        System.out.println("Heyy dames en heertjes!");
     }
 }
